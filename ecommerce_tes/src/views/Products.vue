a8daa26696e626e19cbf8aeabc93b26dcce607a3
AngelFigueiras <angel.figueiras117@gmail.com>
2022-01-23 17:16:02 -0600

    Nav + SearchBar + home

--- ecommerce_tes/src/views/Products.vue
+++ ecommerce_tes/src/views/Products.vue
@@ -1,13 +1,424 @@
 <template>
-  <h1>Esta es la página de productos</h1>
+  <nav class="home-container">
+
+     <!-- Estructura navbar -->
+    <nav class="nav-container">
+        <img src="../assets/icons/icon_menu.svg" alt="menu" class="menu">
+
+        <div class="navbar-left">
+            <img src="../assets/logo_yard_sale.svg" alt="logo" class="navlogo">
+
+            <ul>
+                <li>
+                    <a href="">All</a>
+                </li>
+
+                <li>
+                    <a href="">Geckos</a>
+                </li>
+
+                <li>
+                    <a href="">Snakes</a>
+                </li>
+
+                <li>
+                    <a href="">Reptiles</a>
+                </li>
+
+                <li>
+                    <a href="">Others</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="navbar-right">
+            <ul>
+                <li class="navbar-user" id="user">User</li>
+                <li class="navbar-shopping-cart">
+                    <img src="../assets/icons/icon_shopping_cart.svg" alt="shoppin cart">
+                    <div>3</div>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+
+    <!-- Estructura Search bar -->
+
+    <div class="search-bar">
+
+      <div class="search-container">
+          
+          <form action="/" class="form">
+              <input v-model="email" type="text" id="email" placeholder="Search product" class="search-input">
+          </form>
+  
+      </div>   
+
+    </div>
+
+
+    <!-- Estructura home -->
+    <section class="main-container">
+        <div class="cards-container">
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="https://images.pexels.com/photos/2078845/pexels-photo-2078845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+                
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>gecko</p>
+                    </div>
+
+                    <figure>
+                        <img src="../assets/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    </nav>
 </template>
 
 <script>
+
+// Importar dependencias
+
 export default {
 
 }
 </script>
 
 <style scoped>
+
+/* Estilos nabvar */
+
+.menu{
+    display: none;
+}
+
+.nav-container{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 24px;
+    border-bottom: 1px solid var(--very-light-pink);
+}
+
+.navlogo{
+    width: 100px;
+}
+
+.navbar-user{
+    color: var(--very-light-pink);
+    font-size: var(--sm);
+    margin-right: 16px;
+}
+
+.navbar-left ul,
+.navbar-right ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    height: 60px;
+}
+
+.navbar-left ul li a,
+.navbar-right ul li a{
+    text-decoration: none;
+    color: var(--very-light-pink);
+    border: 1px solid var(--white);
+    padding: 8px;
+    border-radius: 8px;
+}
+
+.navbar-left ul li a:hover,
+.navbar-right ul li a:hover{
+    border: 1px solid var(--hospital-green);
+    color: var(--hospital-green);
+}
+
+.navbar-left{
+    display: flex;
+}
+
+.navbar-left ul{
+    margin-left: 8px;
+}
+
+.navbar-left:nth-child(1){
+    margin-left: 12;
+}
+
+
+.navbar-shopping-cart{
+    position: relative;
+}
+
+.navbar-shopping-cart div{
+    width: 17px;
+    height: 17px;
+    background-color: var(--hospital-green);
+    border-radius: 50%;
+    font-size: var(--sm);
+    font-weight: bold;
+    position: absolute;
+    top: -7px;
+    right: -8px;
+    display: flex;
+    justify-content: center;
+}
+
+/* Estilos searchBar*/
+
+.search-bar{
+  display: none;
+}
+
+.search-container{
+    width: 100%;
+    display: grid;
+    place-items: center;
+    height: 70px;
+    margin-bottom: 10px;
+}
+
+.form{
+    display: flex;
+    flex-direction: row;
+}
+.search-input{
+    height: 32px;
+    width: 300px;
+    padding: 6px;
+    background-color: var(--text-input-field);
+    border-radius: 8px;
+    border: none;
+}
+
+/* Estilos home */
+
+.cards-container{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 240px);
+    gap: 26px;
+    place-content: center;
+}
+
+
+.product-card{
+    width: 240px;
+}
+
+
+.product-card img{
+    width: 240px;
+    height: 240px;
+    border-radius: 20px;
+    object-fit: cover;
+}
+
+.product-info{
+    margin-top: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.product-info figure{
+    margin: 0;
+}
+
+.product-info div p:nth-child(1){
+    font-weight: bold;
+    font-size: var(--md);
+    margin-top: 0;
+    margin-bottom: 4px;
+}
+
+.product-info div p:nth-child(2){
+    font-size: var(--sm);
+    margin-top: 0;
+    margin-bottom: 0;
+    color: var(--very-light-pink);
+}
+
+.product-info figure img{
+    width: 35px;
+    height: 35px;
+}
+
+@media (max-width: 640px) {
+    .cards-container{
+      grid-template-columns: repeat(auto-fill, 140px);
+    } 
+
+    .product-card{
+      width: 140px;
+    }
+    
+    .product-card img{
+      width: 140px;
+      height: 140px;
+    }
+
+    .menu{
+      display: block;
+    }
+
+    .navbar-left ul{
+      display: none;
+    }
+
+    .navbar-user{
+      display: none;
+    }
+
+    .search-bar{
+      display: block;
+    }
+
+}
+
 
 </style>
